8aeac385b8c4739d47ee608f0681cb7b0c353604
Mahesh Chary Indrakanti <imciiit@gmail.com>
2025-07-05 17:35:17 +0530

    added missing files

---
+++ spring/model-attribute/src/test/java/com/imc/model_attribute/ModelAttributeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imc.model_attribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelAttributeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
